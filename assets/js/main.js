42e4b3c1527c0e74a064a99a6a1cc81da944eba0
fatemanazari <fatemanazari78@gmail.com>
2024-03-19 17:30:54 +0330

    Fix linters

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,35 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
-  const header = document.getElementById('header')
+/*= ============== CHANGE BACKGROUND HEADER =============== */
+const scrollHeader = () => {
+  const header = document.getElementById('header');
   // Add a class if the bottom offset is greater than 50 of the viewport
-  this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                     : header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
+  this.scrollY >= 50 ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
 
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
-let mixerPortfolio = mixitup('.work__container', {
+/*= ============== MIXITUP FILTER PORTFOLIO =============== */
+const mixerPortfolio = mixitup('.work__container', {
   selectors: {
-      target: '.work__card'
+    target: '.work__card',
   },
   animation: {
-      duration: 300
-  }
+    duration: 300,
+  },
 });
 
-/* Link active work */ 
-const linkWork = document.querySelectorAll('.work__item')
+/* Link active work */
+const linkWork = document.querySelectorAll('.work__item');
 
-function activeWork(){
-  linework.forEach(L=> L.classList.remove('active-work'))
-  this.classList.add('active-work')
+function activeWork() {
+  linework.forEach((L) => L.classList.remove('active-work'));
+  this.classList.add('active-work');
 }
 
-linkWork.forEach(L=> L.addEventListener('click', activeWork))
-/*=============== SWIPER TESTIMONIAL ===============*/
+linkWork.forEach((L) => L.addEventListener('click', activeWork));
+/*= ============== SWIPER TESTIMONIAL =============== */
 
+/*= ============== SCROLL SECTIONS ACTIVE LINK =============== */
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/*= ============== LIGHT DARK THEME =============== */
 
-
-/*=============== LIGHT DARK THEME ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-
+/*= ============== SCROLL REVEAL ANIMATION =============== */
